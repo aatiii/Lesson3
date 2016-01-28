131892d0c1a1372f779aa0503322ffe034505a45
aatiii <helen_ati@163.com>
2016-01-28 10:51:36 -0500

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+// main javascrip document
+
+"use strict";
+
+var username = "Helen";
+var age = 23;
+
+var usernames = [];
+usernames = ["Mike", "Rich", "Anju"];
+// var usernames = new Array();
+
+//usernames[0] = "Mike";
+console.log(usernames);
+
+usernames.push("Tom");
