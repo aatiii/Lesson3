69d2f28c08bb6545ddf7a60ec80e47de992079c1
aatiii <helen_ati@163.com>
2016-01-28 11:54:01 -0500

    updated index file and app.js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,6 +6,9 @@ var username = "Helen";
 var age = 23;
 
 var usernames = [];
+
+
+/*
 usernames = ["Mike", "Rich", "Anju"];
 // var usernames = new Array();
 
@@ -13,3 +16,17 @@ usernames = ["Mike", "Rich", "Anju"];
 console.log(usernames);
 
 usernames.push("Tom");
+*/
+
+
+// Initialize the array with values
+usernames = ["Mike", "Rich", "Anju", "Margot"];
+console.log("before the push: " + usernames);
+
+//add an element to the bottom of the array
+usernames.push("Tom");
+console.log("after the push: " + usernames);
+
+//remove an element from the bottom of the array
+usernames.pop();
+console.log("after the pop: " + usernames);
